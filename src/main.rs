d2ec9239cd8e9edba8819268ea076412c9180ad0
ru32noq <a.lankheit@campus.lmu.de>
2020-11-30 16:56:46 +0100

    Working application

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 extern crate iced;
 
-use iced::button;
+use iced::{button, executor};
 use iced::Length;
-use iced::{Button, Column, Element, Row, Sandbox, Settings, Space, Svg, Text};
-use std::collections::HashMap;
+use iced::{Button, Column, Element, Row, Application, Settings, Space, Svg, Text, Command, Subscription};
+use std::collections::{HashMap, HashSet};
+use iced_native::{
+    window::Event,
+};
+
+
+use std::path::PathBuf;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum CellType {
@@ -17,49 +23,83 @@ fn main() -> iced::Result {
     Game::run(Settings::default())
 }
 
-struct Game {
+struct Field {
     cells: Vec<Vec<CellType>>,
+    row_counts: Vec<usize>,
+    column_counts: Vec<usize>,
+}
+
+fn solve_puzzle(tents: &HashSet<(usize, usize)>, row_counts: &Vec<usize>, column_counts: &Vec<usize>) {
+
+}
+
+struct Game {
+    field: Field,
+
     svgs: HashMap<CellType, Svg>,
     width: u16,
     height: u16,
 }
 
-#[derive(Debug, Copy, Clone)]
-enum Message {
-    Pressed,
+impl Game {
+
 }
 
-impl Sandbox for Game {
-    type Message = Message;
 
-    fn new() -> Self {
+#[derive(Debug, Clone)]
+enum Message {
+    FileDropped(PathBuf),
+    SolvePuzzle,
+}
+
+impl Application for Game {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
         let cells = vec![
             vec![CellType::Tent, CellType::Meadow],
             vec![CellType::Tree, CellType::Unknown],
         ];
+
+        let field = Field {
+            cells: cells,
+            row_counts: vec![2, 1],
+            column_counts: vec![0,1],
+        };
 
         let mut svgs = HashMap::new();
         let _ = svgs.insert(CellType::Meadow, Svg::from_path("images/meadow.svg"));
         let _ = svgs.insert(CellType::Tent, Svg::from_path("images/tent.svg"));
         let _ = svgs.insert(CellType::Tree, Svg::from_path("images/tree.svg"));
 
-        Game {
-            cells,
+        let game = Game {
+            field,
             svgs,
             width: 30,
             height: 30,
-        }
+        };
+        (game, Command::none())
     }
 
     fn title(&self) -> String {
-        String::from("Tents")
+        String::from("Solving Tents")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::FileDropped(path) => println!("{:?}", path),
+            Message::SolvePuzzle => println!("Solving puzzle ..."),
+        };
+        Command::none()
     }
 
     fn view(&mut self) -> Element<Self::Message> {
         let mut grid = Column::new();
-        for row in self.cells.iter() {
+        for (i, row) in self.field.cells.iter().enumerate() {
             let mut columns = Row::new();
-            for column in row.iter() {
+            for (j, column) in row.iter().enumerate() {
                 columns = columns.push({
                     let element: Element<Self::Message> = if let Some(svg) = self.svgs.get(column) {
                         svg.clone()
@@ -73,10 +113,27 @@ impl Sandbox for Game {
                     element
                 }).spacing(2)
             }
-            grid = grid.push(columns).spacing(2)
+            grid = grid.push(columns.push(Text::new(self.field.column_counts[i].to_string())))
+                .spacing(2)
         }
-        grid.into()
+        grid.push({
+            let elements = self.field.row_counts.iter().map(|number|
+                Text::new(number.to_string())
+                    .width(Length::Units(self.width))
+                    .height(Length::Units(self.height))
+                    .into()
+            ).collect();
+            Row::with_children(elements)
+        }).into()
     }
 
-    fn update(&mut self, _: Self::Message) {}
+    fn subscription(&self) -> Subscription<Self::Message> {
+        iced_native::subscription::events_with(
+            |event, _| {
+                match event {
+                    iced_native::Event::Window(Event::FileDropped(path)) => Some(Message::FileDropped(path)),
+                    _ => None
+                }
+            })
+    }
 }
