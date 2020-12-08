552d576db57c9fdb3cea1fb930b779acea977799
Korbinian Federholzner <k.federholzner@campus.lmu.de>
2020-12-08 12:19:22 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ mod game;
 mod field_widget;
 mod message;
 mod control_widget;
-mod puzzle_creation;
 
-use puzzle_creation::create_random_puzzle;
 use game::Game;
 
 use iced::{Application, Settings};
