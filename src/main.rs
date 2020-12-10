4dbfedd3cd90467e8b518f65d40172ee43163a9e
ru32noq <a.lankheit@campus.lmu.de>
2020-12-10 10:29:03 +0100

    Fixed main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ mod field_widget;
 mod message;
 mod control_widget;
 mod random_creation_widget;
-mod cnf;
-mod formula;
 mod puzzle_creation;
 mod number_input;
 
@@ -15,7 +13,5 @@ use iced::{Application, Settings};
 
 fn main() -> iced::Result {
     let v = vec![1,2,3,4];
-    //field::Field::axis_constraint(&v, 2);
-    //Ok(())
     Game::run(Settings::default())
 }
