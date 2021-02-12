ed174558f3ef07d8ed593c9e2f50d8900e2d8349
Alexander Lankheit <a.lankheit@campus.lmu.de>
2021-02-12 23:21:03 +0100

    Remove panic from solver-bench

--- solver-bench/src/config.rs
+++ solver-bench/src/config.rs
@@ -2,8 +2,8 @@ use solver::solvers::InterruptibleSolver;
 use std::path::PathBuf;
 
 pub struct Config {
-    pub input:          String,
-    pub solvers:        Vec<(String, Box<dyn InterruptibleSolver>)>,
-    pub output:         PathBuf,
-    pub max_duration:   u64,
+    pub input: String,
+    pub solvers: Vec<(String, Box<dyn InterruptibleSolver>)>,
+    pub output: PathBuf,
+    pub max_duration: u64,
 }
