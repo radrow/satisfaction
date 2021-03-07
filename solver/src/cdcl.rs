d34cc119506eb80ec87e72117660c3722b2e71b8
Alexander Lankheit <a.lankheit@campus.lmu.de>
2021-03-07 15:18:05 +0100

    Generalise branching, learning, and deletion

--- solver/src/cdcl.rs
+++ solver/src/cdcl.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::{collections::{HashSet, VecDeque}, ops::Not};
+use std::{borrow::BorrowMut, cell::RefCell, collections::{HashSet, VecDeque}, marker::PhantomData, ops::Not, rc::Rc};
 
 type IndexSet<V> = indexmap::IndexSet<V, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
 
@@ -44,41 +44,91 @@ impl Clause {
 type Variables = Vec<Variable>;
 type Clauses = Vec<Clause>;
 
-trait BranchingStrategy {
-    fn pick_literal(&self, clauses: &Clauses, variables: &Variables) -> Option<CNFVar>;
+
+trait Update {
+    fn on_assign(&mut self, variable: VariableId, clauses: &Clauses, variables: &Variables) {}
+    fn on_unassign(&mut self, variable: VariableId, clauses: &Clauses, variables: &Variables) {}
+    fn on_learn(&mut self, learned_clause: &Clause, clauses: &Clauses, variables: &Variables) {}
+    fn on_conflict(&mut self, empty_clause: ClauseId, clauses: &Clauses, variables: &Variables) {}
+    fn on_deletion(&mut self, deleted_clause: &Clause) {}
 }
 
-trait LearningScheme {
-    fn find_conflict_clause(&self, empty_clause: ClauseId, branching_depth: usize, clauses: &Clauses, variables: &Variables) -> (CNFClause, usize);
+trait Initialisation {
+    fn initialise(clauses: &Clauses, variables: &Variables) -> Self where Self: Sized;
 }
 
-trait ClauseDeletionStrategy {
-    fn delete_clause(&self, clauses: &mut Clauses, variables: &mut Variables);
+trait BranchingStrategy: Initialisation+Update {
+    fn pick_literal(&mut self, clauses: &Clauses, variables: &Variables) -> Option<CNFVar>;
 }
 
-struct ExecutionState {
+trait LearningScheme: Initialisation+Update {
+    fn find_conflict_clause(&mut self, empty_clause: ClauseId, branching_depth: usize, clauses: &Clauses, variables: &Variables) -> (CNFClause, usize);
+}
+
+trait ClauseDeletionStrategy: Initialisation+Update {
+    fn delete_clause(&mut self, clauses: &mut Clauses, variables: &mut Variables);
+}
+
+
+struct ExecutionState<B,L,C>
+where B: 'static+BranchingStrategy,
+      L: 'static+LearningScheme,
+      C: 'static+ClauseDeletionStrategy {
+
     clauses: Clauses,
     variables: Variables,
     branching_depth: usize,
     unit_queue: VecDeque<CNFVar>,
+
+    branching_strategy: Rc<RefCell<B>>,
+    learning_scheme: Rc<RefCell<L>>,
+    clause_deletion_strategy: Rc<RefCell<C>>,
+
+    updates: Vec<Rc<RefCell<dyn Update>>>,
 }
 
-impl ExecutionState {
-    fn new(formula: &CNF) -> ExecutionState {
-        unimplemented!()
+impl<B,L,C> ExecutionState<B,L,C>
+where B: BranchingStrategy,
+      L: LearningScheme,
+      C: ClauseDeletionStrategy {
+
+    fn new(formula: &CNF) -> ExecutionState<B, L, C> {
+        let unit_queue = VecDeque::new();
+
+        let clauses = Clauses::new();
+        let variables = Variables::new();
+
+        let branching_strategy = Rc::new(RefCell::new(B::initialise(&clauses, &variables)));
+        let learning_scheme = Rc::new(RefCell::new(L::initialise(&clauses, &variables)));
+        let clause_deletion_strategy = Rc::new(RefCell::new(C::initialise(&clauses, &variables)));
+
+        ExecutionState {
+            clauses,
+            variables,
+            branching_depth: 0,
+            unit_queue,
+
+            updates: vec![
+                branching_strategy.clone(),
+                learning_scheme.clone(),
+                clause_deletion_strategy.clone(),
+            ],
+
+            branching_strategy,
+            learning_scheme,
+            clause_deletion_strategy,
+        }
     }
 
-    fn cdcl(
-        mut self,
-        branching_strategy: &impl BranchingStrategy,
-        learning_scheme: &impl LearningScheme,
-        clause_deletion_strategy: &impl ClauseDeletionStrategy,
-    ) -> SATSolution {
+    fn cdcl(mut self) -> SATSolution {
         if self.unit_propagation().is_some() {
             return SATSolution::Unsatisfiable;
         }
 
-        while let Some(literal) = branching_strategy.pick_literal(&self.clauses, &self.variables) {
+        while let Some(literal) = { 
+            let mut bs = self.branching_strategy.as_ref().borrow_mut();
+            bs.pick_literal(&self.clauses, &self.variables)
+        } {
             // Try to set a variable or receive the conflict clause it was not possible
             if self
                 .set_variable(literal, false)
@@ -86,7 +136,7 @@ impl ExecutionState {
                 .or(self.unit_propagation())
                 // If there was a conflict backtrack; return true if backtracking failed
                 .map_or(false, |conflict_clause| {
-                    self.backtracking(conflict_clause, learning_scheme)
+                    self.backtracking(conflict_clause)
                 })
             {
                 return SATSolution::Unsatisfiable;
@@ -120,22 +170,33 @@ impl ExecutionState {
     fn backtracking(
         &mut self,
         empty_clause: ClauseId,
-        learning_scheme: &impl LearningScheme,
     ) -> bool {
-        let (conflict_clause, assertion_level) = learning_scheme.find_conflict_clause(empty_clause, self.branching_depth, &self.clauses, &self.variables);
+        self.updates.iter()
+            .for_each(|up| up.as_ref().borrow_mut().on_conflict(empty_clause, &self.clauses, &self.variables));
+
+        let (conflict_clause, assertion_level) = {
+            let mut ls = self.learning_scheme.as_ref().borrow_mut();
+            ls.find_conflict_clause(empty_clause, self.branching_depth, &self.clauses, &self.variables)
+        };
 
         let conflict_clause = Clause::new(conflict_clause.into_iter(), &mut self.variables);
+        self.updates.iter()
+            .for_each(|up| up.as_ref().borrow_mut().on_learn(&conflict_clause, &self.clauses, &self.variables));
         self.clauses.push(conflict_clause);
 
         // TODO: More efficient: e.g. Stack + dropWhile
-        for variable in self.variables.iter_mut() {
-            match variable.assignment {
+        for id in 0..self.variables.len() {
+            match self.variables[id].assignment {
                 Some(assign) if assign.branching_level > assertion_level => {
-                    variable.assignment = None;
+                    self.variables[id].assignment = None;
+
+                    // 
+                    self.updates.iter().for_each(|up| up.as_ref().borrow_mut().on_unassign(id, &self.clauses, &self.variables));
                 },
                 _ => {},
             }
         }
+
         self.branching_depth = assertion_level;
         self.unit_queue.clear();
 
@@ -151,7 +212,7 @@ impl ExecutionState {
         
 
         self.unit_propagation()
-            .map_or(false, |empty_clause| self.backtracking(empty_clause, learning_scheme))
+            .map_or(false, |empty_clause| self.backtracking(empty_clause))
     }
 
     fn is_satisfied(&self) -> bool {
@@ -162,11 +223,19 @@ impl ExecutionState {
 
 struct RealSAT;
 
+impl Initialisation for RealSAT {
+    fn initialise(_clauses: &Clauses, _variables: &Variables) -> Self where Self: Sized {
+        RealSAT
+    }
+}
+impl Update for RealSAT {}
+
+
 impl LearningScheme for RealSAT {
-    fn find_conflict_clause(&self, empty_clause: ClauseId, branching_depth: usize, clauses: &Clauses, variables: &Variables) -> (CNFClause, usize) {
+    fn find_conflict_clause(&mut self, empty_clause: ClauseId, branching_depth: usize, clauses: &Clauses, variables: &Variables) -> (CNFClause, usize) {
         // TODO: Optimize preallocation
         // Start with vertices that are connected to the conflict clause
-        let mut literal_queue: IndexSet<usize> = clauses[empty_clause].literals
+        let mut literal_queue: IndexSet<VariableId> = clauses[empty_clause].literals
             .iter()
             .map(|lit| lit.id)
             .collect();
@@ -197,20 +266,24 @@ impl LearningScheme for RealSAT {
 }
 
 
-struct CDCLSolver<B: BranchingStrategy, L: LearningScheme, C: ClauseDeletionStrategy> {
-    branching_strategy: B,
-    learning_scheme: L,
-    clause_deletion_strategy: C,
+struct CDCLSolver<B,L,C>
+where B: BranchingStrategy,
+      L: LearningScheme,
+      C: ClauseDeletionStrategy {
+
+    branching_strategy: PhantomData<B>,
+    learning_scheme: PhantomData<L>,
+    clause_deletion_strategy: PhantomData<C>,
 }
 
 impl<B, L, C> Solver for CDCLSolver<B, L, C>
 where
-    B: BranchingStrategy,
-    L: LearningScheme,
-    C: ClauseDeletionStrategy,
+    B: 'static+BranchingStrategy,
+    L: 'static+LearningScheme,
+    C: 'static+ClauseDeletionStrategy,
 {
     fn solve(&self, formula: &CNF) -> SATSolution {
-        let execution_state = ExecutionState::new(formula);
-        execution_state.cdcl(&self.branching_strategy, &self.learning_scheme, &self.clause_deletion_strategy)
+        let execution_state = ExecutionState::<B, L, C>::new(formula);
+        execution_state.cdcl()
     }
 }
